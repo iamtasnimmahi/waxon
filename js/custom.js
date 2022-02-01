881c80b32ea23798c258b695f817dc9acdd17373
Tasnim Mahi <43760529+iamtasnimmahi@users.noreply.github.com>
2022-02-01 14:37:25 +0600

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,23 @@
+$(function(){
+
+    // banner part start
+    $('.banner_slick').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 500,
+        arrows:false,
+      });
+    // banner part end
+    
+    // service part start
+    $('.service_slick').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 500,
+        arrows:false,
+      });
+    // service part end
+
+});
